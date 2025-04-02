503c2f3c25d1f57c309f4a895210c6664eb60e0f
crispin <h.c.shin.dev09@gmail.com>
2025-04-03 00:08:43 +0900

    :sparkles: Feat: add test list variable

--- terraform/aws/crispin-lab/core/global/global-ci-test.tf
+++ terraform/aws/crispin-lab/core/global/global-ci-test.tf
@@ -19,7 +19,7 @@ variable "test_bool_var" {
 variable "test_list_var" {
   description = "테스트용 리스트 변수"
   type        = list(string)
-  default     = ["기본값1", "기본값2"]
+  default     = ["기본값1", "기본값2", "기본값3"]
 }
 
 variable "test_map_var" {
